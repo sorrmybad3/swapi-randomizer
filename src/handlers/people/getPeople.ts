27f5376be893e2c28f405f8c30bd6b7251c08b73
sorrmybad3 <smbad3@outlook.com>
2024-10-31 07:44:19 -0500

    serverless.ym and getPeople handler placeholder added.

---
+++ src/handlers/people/getPeople.ts
@@ -0,0 +1,13 @@
+import { APIGatewayEvent } from "aws-lambda";
+
+async function handler(event: APIGatewayEvent) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Hello from getPeople',
+      event,
+    }),
+  }
+}
+
+module.exports = { handler };
